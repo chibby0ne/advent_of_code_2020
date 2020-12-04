7387ce61e021ceef5b0916dbe0c72f76bdd46ef5
Antonio Gutierrez <chibby0ne@gmail.com>
2020-12-04 04:49:19 +0100

    day2: refactor part1
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- day2/part1/src/main.rs
+++ day2/part1/src/main.rs
@@ -3,21 +3,23 @@ use std::io::prelude::*;
 
 fn main() {
     let stdin = io::stdin();
-    let vec: Vec<String> = stdin
-        .lock()
-        .lines()
-        .filter_map(Result::ok)
-        .collect();
-    let count = vec.iter().filter(|&x| {
-        let mut iter = x.split_whitespace();
-        let range: Vec<_> = iter.next().unwrap().split("-").filter_map(|x| x.parse::<usize>().ok()).collect();
-        let char_to_find = iter.next().unwrap().chars().nth(0).unwrap();
-        let string = iter.next().unwrap();
-        if string.matches(char_to_find).count() >= *range.get(0).unwrap() && string.matches(char_to_find).count() <= *range.get(1).unwrap() {
-            true
-        } else {
-            false
-        }
-    }).count();
+    let vec: Vec<String> = stdin.lock().lines().filter_map(Result::ok).collect();
+    let count = vec
+        .iter()
+        .filter(|&x| {
+            // line looks like this:
+            // 1-3 a: abcde
+            let mut iter = x.split_whitespace();
+            let mut range_iter = iter
+                .next()
+                .unwrap()
+                .split('-')
+                .filter_map(|x| x.parse::<usize>().ok());
+            let (min, max) = (range_iter.next().unwrap(), range_iter.next().unwrap());
+            let char_to_find = iter.next().unwrap().chars().next().unwrap();
+            let count = iter.next().unwrap().matches(char_to_find).count();
+            count >= min && count <= max
+        })
+        .count();
     println!("Number of valid passwords: {}", count);
 }
