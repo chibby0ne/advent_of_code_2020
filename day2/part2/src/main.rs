7a89b39466fba9a73f42bd4d132439ae85c1f0f1
Antonio Gutierrez <chibby0ne@gmail.com>
2020-12-04 05:17:17 +0100

    day2: reformat part2
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- day2/part2/src/main.rs
+++ day2/part2/src/main.rs
@@ -18,9 +18,8 @@ fn main() {
             let (first_pos, second_pos) = (pos_iter.next().unwrap(), pos_iter.next().unwrap());
             let char_to_find = iter.next().unwrap().chars().next().unwrap();
             let string = iter.next().unwrap();
-            let output = (string.chars().nth(first_pos - 1) == Some(char_to_find)) ^ (string.chars().nth(second_pos - 1) == Some(char_to_find));
-            // println!("for {}, it is {}", x, if output { "valid" } else { "invalid" });
-            output
+            (string.chars().nth(first_pos - 1) == Some(char_to_find))
+                ^ (string.chars().nth(second_pos - 1) == Some(char_to_find))
         })
         .count();
     println!("Number of valid passwords: {}", count);
