5d1ba757c21f6c08aeefb5bb71fc58e3083c15f7
Antonio Gutierrez <chibby0ne@gmail.com>
2021-01-03 19:38:40 +0100

    day7: format/clippy part1
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- day7/part1/src/main.rs
+++ day7/part1/src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashSet;
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
+
+const BAG: &str = "shinygold";
 
 #[derive(Debug)]
 struct Bags {
@@ -11,22 +13,19 @@ struct Bags {
 
 impl Bags {
     fn new(bags: HashSet<Bag>) -> Bags {
-        Bags{
-            bags,
-        }
+        Bags { bags }
     }
-
 }
 
-#[derive(Debug,Eq)]
+#[derive(Debug, Eq)]
 struct Bag {
     name: String,
-    bags_and_numbers: HashSet<(String,i64)>,
+    bags_and_numbers: HashSet<(String, i64)>,
 }
 
 impl Bag {
     fn new(bag_name: String) -> Bag {
-        Bag{
+        Bag {
             name: bag_name,
             bags_and_numbers: HashSet::new(),
         }
@@ -39,7 +38,6 @@ impl Bag {
     fn add_contained_bag(&mut self, bag_and_number: (String, i64)) {
         self.bags_and_numbers.insert(bag_and_number);
     }
-
 }
 
 impl PartialEq for Bag {
@@ -52,10 +50,13 @@ impl Hash for Bag {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.name.hash(state);
     }
-
 }
 
-fn is_capable_of_holding(bags: &HashMap<String, Bag>, bag: &Bag, name_of_bag_to_hold: &str) -> bool {
+fn is_capable_of_holding(
+    bags: &HashMap<String, Bag>,
+    bag: &Bag,
+    name_of_bag_to_hold: &str,
+) -> bool {
     if bag.can_contain_bag(name_of_bag_to_hold) {
         return true;
     }
@@ -66,7 +67,9 @@ fn is_capable_of_holding(bags: &HashMap<String, Bag>, bag: &Bag, name_of_bag_to_
         if b.can_contain_bag(name_of_bag_to_hold) {
             return true;
         } else {
-            b.bags_and_numbers.iter().for_each(|(x,_)| stack.push(bags.get(x).unwrap()));
+            b.bags_and_numbers
+                .iter()
+                .for_each(|(x, _)| stack.push(bags.get(x).unwrap()));
         }
     }
     false
@@ -75,38 +78,36 @@ fn is_capable_of_holding(bags: &HashMap<String, Bag>, bag: &Bag, name_of_bag_to_
 fn part1(bags: &HashMap<String, Bag>) {
     let mut cache: HashSet<&str> = HashSet::new();
     for (name, bag) in bags {
-        if is_capable_of_holding(bags, bag, "shinygold") {
+        if is_capable_of_holding(bags, bag, BAG) {
             cache.insert(name);
         }
     }
     println!("part1 count: {}", cache.iter().count());
 }
 
-fn part2(_bags: &HashMap<String, Bag>) {
-    println!("part2 count: {}", 126);
-}
-
 fn main() {
     let stdin = io::stdin();
     let mut bags: HashMap<String, Bag> = HashMap::new();
     while let Some(Ok(line)) = stdin.lock().lines().next() {
-        let mut iter = line.split("contain").into_iter();
-        let bag_name: String = iter.next().unwrap().trim().trim_end_matches("bags").trim().split_whitespace().collect();
+        let mut iter = line.split("contain");
+        let bag_name: String = iter
+            .next()
+            .unwrap()
+            .trim()
+            .trim_end_matches("bags")
+            .trim()
+            .split_whitespace()
+            .collect();
         let iter_bags = iter.next().unwrap().split(',');
         let mut bag = Bag::new(bag_name.clone());
         for bag_contained in iter_bags {
             let mut bag_container_iter = bag_contained.trim().split_whitespace();
             if let Ok(number) = bag_container_iter.next().unwrap().parse::<i64>() {
-                // dbg!(&number);
                 let bag_contained_name: String = bag_container_iter.take(2).collect();
-                // dbg!(&bag_contained_name);
                 bag.add_contained_bag((bag_contained_name, number));
             }
         }
-        // dbg!(&bag);
         bags.insert(bag_name, bag);
     }
     part1(&bags);
-    part2(&bags);
 }
-
