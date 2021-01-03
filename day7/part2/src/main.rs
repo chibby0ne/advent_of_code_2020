b5fe1de1d2e2fbc2a49ada9ab282bf76058fe332
Antonio Gutierrez <chibby0ne@gmail.com>
2021-01-03 19:45:42 +0100

    day7: format/clippy part2
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- day7/part2/src/main.rs
+++ day7/part2/src/main.rs
@@ -1,8 +1,8 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashSet;
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
 
 #[derive(Debug)]
 struct Bags {
@@ -11,35 +11,27 @@ struct Bags {
 
 impl Bags {
     fn new(bags: HashSet<Bag>) -> Bags {
-        Bags{
-            bags,
-        }
+        Bags { bags }
     }
-
 }
 
-#[derive(Debug,Eq)]
+#[derive(Debug, Eq)]
 struct Bag {
     name: String,
-    bags_and_numbers: HashSet<(String,i64)>,
+    bags_and_numbers: HashSet<(String, i64)>,
 }
 
 impl Bag {
     fn new(bag_name: String) -> Bag {
-        Bag{
+        Bag {
             name: bag_name,
             bags_and_numbers: HashSet::new(),
         }
     }
 
-    fn can_contain_bag(&self, bag: &str) -> bool {
-        self.bags_and_numbers.iter().any(|(x, _)| x == bag)
-    }
-
     fn add_contained_bag(&mut self, bag_and_number: (String, i64)) {
         self.bags_and_numbers.insert(bag_and_number);
     }
-
 }
 
 impl PartialEq for Bag {
@@ -52,20 +44,16 @@ impl Hash for Bag {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.name.hash(state);
     }
-
 }
 
 fn calculate_bags_inside(bags: &HashMap<String, Bag>, bag_name: &String, sum_so_far: i64) -> i64 {
     let bag = bags.get(bag_name).unwrap();
     let mut sum = sum_so_far;
     for (inside_bag_name, inside_bag_num) in &bag.bags_and_numbers {
-        // dbg!(inside_bag_name);
-        // dbg!(inside_bag_num);
         sum += calculate_bags_inside(bags, &inside_bag_name, sum_so_far * inside_bag_num)
     }
     sum
 }
-
 
 fn part2(bags: &HashMap<String, Bag>) {
     let mut sum_outside = 0;
@@ -80,8 +68,15 @@ fn main() {
     let stdin = io::stdin();
     let mut bags: HashMap<String, Bag> = HashMap::new();
     while let Some(Ok(line)) = stdin.lock().lines().next() {
-        let mut iter = line.split("contain").into_iter();
-        let bag_name: String = iter.next().unwrap().trim().trim_end_matches("bags").trim().split_whitespace().collect();
+        let mut iter = line.split("contain");
+        let bag_name: String = iter
+            .next()
+            .unwrap()
+            .trim()
+            .trim_end_matches("bags")
+            .trim()
+            .split_whitespace()
+            .collect();
         let iter_bags = iter.next().unwrap().split(',');
         let mut bag = Bag::new(bag_name.clone());
         for bag_contained in iter_bags {
@@ -95,4 +90,3 @@ fn main() {
     }
     part2(&bags);
 }
-
